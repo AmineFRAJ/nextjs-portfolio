93c145db27c7815bd16a53d957ffb8c92ff63576
AmineFRAJ <amine0924@gmail.com>
2024-09-15 22:02:47 +0100

    update contact page

--- app/Resume/page.jsx
+++ app/Resume/page.jsx
@@ -143,7 +143,8 @@ const Resume = () => {
             {/* certif */}
             <TabsContent value="certifications" className="  w-full">
               <div className=" flex flex-col gap-[30px] text-center  xl:text-left">
-                <h3 className="text-4xl font bold">{certifications.title}</h3>
+                <h3 className="text-4xl font bold">
+                  {certifications.title}</h3>
                 <p className="max-w-[600px] text-white/60  mx-auto xl:mx-0 lg:text-justify">
                   {certifications.description}
                 </p>
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,78 +1,107 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Description } from "@radix-ui/react-dialog";
+import { motion } from "framer-motion";
+import { FaPhoneAlt, FaMapMarkedAlt, FaEnvelope } from "react-icons/fa";
+
+const info = [
+  { icon: <FaPhoneAlt />, title: "Phone", Description: "(+216) 52 680 566" },
+  { icon: <FaEnvelope />, title: "Email", Description: "amine0924@gmail.com" },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: "Adress",
+    Description: "16 Rue Omar Mokhtar, Ariana",
+  },
+];
 const Contact = () => {
   return (
-    <section className="h-full">
-      <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          {/*  Contact Form */}
-          <div className="text-center xl:text-left">
-            <h1 className="h1 mb-6">
-              Contact Me
-            </h1>
-            <form className="max-w-[500px] space-y-4">
-              <div>
-                <label htmlFor="name" className="block text-sm font-medium">
-                  Your Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  className="w-full border border-gray-300 p-2 rounded-md"
-                  placeholder="Enter your name"
-                  required
-                />
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2  xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Contact Me</h3> {/* input*/}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname"  />
+                <Input type="lastname" placeholder="Lastname"  />
+                <Input type="email" placeholder="Email" required/>
+                <Input type="phone" placeholder="Phone" />
               </div>
-
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium">
-                  Your Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  className="w-full border border-gray-300 p-2 rounded-md"
-                  placeholder="Enter your email"
-                  required
-                />
-              </div>
-
-              <div>
-                <label htmlFor="message" className="block text-sm font-medium" >
-                  Your Message
-                </label>
-                <textarea
-                  id="message"
-                  className="w-full border border-gray-300 p-2 rounded-md"
-                  rows="4"
-                  placeholder="Enter your message"
-                  required
-                />
-              </div>
-
-              <Button className="bg-accent text-black px-4 py-2 rounded-md hover:bg-green-600 transition-all">
-  Send Message
-</Button>
-
+              {/* select*/}
+              {/* <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="web-dev">Web Development</SelectItem>
+                    <SelectItem value="manual-testing">
+                      Manual Testing
+                    </SelectItem>
+                    <SelectItem value="ui-design">UI Design</SelectItem>
+                    
+                  </SelectGroup>
+                </SelectContent>
+              </Select> */}
+              {/*Textarea*/}
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your message here."
+                rows={6}
+                required
+              />
+              {/* btn*/}
+              <Button
+                variant="outline"
+                className="  max-w-40 flex font-thin text-accent items-center gap-2 bg-dark border-accent rounded-2xl hover:font-bold "
+              >
+                Send message
+              </Button>
             </form>
           </div>
-
-          {/* my contact*/}
-          <div className="mt-12 xl:mt-0 text-center xl:text-left">
-            <h2 className="text-2xl font-bold">Mohamed Amine FRAJ </h2>
-            <p className="mt-4">
-              <strong>Email:</strong> amine0924@gmail.com
-            </p>
-            <p className="mt-4">
-              <strong>Phone:</strong> +21652680566
-            </p>
-            <p className="mt-4">
-              <strong>Address:</strong> 16 Rue Omar Mokhtar, Ariana, Tunisia
-            </p>
+          {/* info*/}
+          <div className=" flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex gap-6 items-center">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">
+                        {item.title}
+                      </p>
+                      <h3 className="text-xl">{item.Description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
